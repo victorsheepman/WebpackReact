007802ff4abfadf8c67b3b9edb3c3bf6fbc2f7c9
Victor Marquez <chirlychplin@gmail.com>
2021-04-09 09:29:44 -0400

    Error de build solucionado

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const {cleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
@@ -54,7 +54,7 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: '[name].css'
         }),
-        new cleanWebpackPlugin(),
+        new CleanWebpackPlugin(),
     ],
     optimization: {
         minimize: true,
